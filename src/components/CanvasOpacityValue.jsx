3c7baa329e53636aa413e4e55461207c2c70e0dd
camerontrader <camerondtrader@gmail.com>
2023-07-25 15:58:20 -0700

    local scaling and 0 set to white

--- src/components/CanvasOpacityValue.jsx
+++ src/components/CanvasOpacityValue.jsx
@@ -1,4 +1,4 @@
-import { scaleLinear } from "d3-scale";
+// import { scaleLinear } from "d3-scale";
 
 // A canvas rendering function that renders a single colored rectangle for each data point
 // with opacity scaled to the max of the order
@@ -24,6 +24,11 @@ export default function CanvasSimpleValueComponent({ canvasRef, state, scales, l
     if(!min.length && min < 0) min = 0;
     let max = meta["max"]
 
+    // console.log(...data.map((d) => {
+    //   return d.data['mean']
+    // }))
+    
+
     // rendering constants
     let t = {...transform}
 
@@ -33,11 +38,63 @@ export default function CanvasSimpleValueComponent({ canvasRef, state, scales, l
     const sw = step * (1 - strokeWidthMultiplier);
     const rw = sizeScale(sw) * t.k - 1
 
+    // function that scales input to alpha values linearly, but 
+    // input values equal to 0 are set to 0
+    // thanks ChatGPT!!
+    function scaleLinearAboveThresh(threshold) {
+      let domain = [min, max]
+      let range = [0.2, 1]
+
+      function scale(d) {
+        if (d > threshold) {
+          const dScaled = (d - domain[0]) / (domain[1] - domain[0])
+          const dInRange = (range[1] - range[0]) * dScaled + range[0]
+          return dInRange
+        } else {
+          return 0
+        }
+      }
+      scale.domain = function (newDomain=null) {
+        if (newDomain) {
+          domain = [...newDomain]
+          return scale
+        } else {
+          return [...domain]
+        }
+      }
+      scale.range = function (newRange=null) {
+        if (newRange) {
+          range = [...newRange]
+          return scale
+        } else {
+          return [...range]
+        }
+      }
+      return scale
+    }
+
     let domain = [min, max]
-    let alphaScale = scaleLinear()
+    // console.log(domain)
+    let alphaScale = scaleLinearAboveThresh(0)
       .domain(domain)
-      .range([0.2, 1]) 
+      .range([0.2, 1]);
+
+    // console.log(alphaScale.domain())
+
+    // let alphaScale = scaleLinear()
+    //   .domain(domain)
+    //   .range([0.2, 1])
     
+    let localMax = 0
+    let localMin = 0
+    for(i = 0; i < data.length; i++) {
+      d = data[i];
+      if(d.data) {
+        const sample = fieldChoice(d);
+        localMax = Math.max(localMax, sample.value)
+      }
+    }
+    alphaScale.domain([[localMin], [localMax]])
     for(i = 0; i < data.length; i++) {
       d = data[i];
       // scale and transform the coordinates
@@ -47,11 +104,12 @@ export default function CanvasSimpleValueComponent({ canvasRef, state, scales, l
       if(d.data) {
         const sample = fieldChoice(d);
         if(sample) {
-          if(min.length) {
-            let fi = fields.indexOf(sample.field)
-            domain = [min[fi] < 0 ? 0 : min[fi], max[fi]]
-            alphaScale.domain(domain)
-          }
+          // if(min.length) {
+            // let fi = fields.indexOf(sample.field)
+            // domain = [min[fi] < 0 ? 0 : min[fi], max[fi]]
+            // alphaScale.domain(domain)
+            // alphaScale.domain([[localMin], [localMax]])
+          // }
           let a = alphaScale(sample.value)
           ctx.globalAlpha = a < 0 ? 0 : a
           ctx.fillStyle = fieldColor(sample.field)
