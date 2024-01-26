4b888af6674998e8554e5d82330957a72c407a19
camerontrader <camerondtrader@gmail.com>
2024-01-25 17:26:36 -0800

    comments

--- src/components/Narration/CrossScaleNarration.jsx
+++ src/components/Narration/CrossScaleNarration.jsx
@@ -65,10 +65,13 @@ export default async function CrossScaleNarration(selected, fetchLayerData, laye
     return topFieldsAcrossOrders
   }
 
+  // path based approach where the each suggested segment is within the one an order below it
   if(selected && method == 'path') {
     // get the top field within each layer for all overlapping segments
     let topFieldsAllLayers = Promise.all(layers.map((layer, i) => {
+      // from order of selected segment to 14...
       let ordersUp = orders.slice(selected.order - Math.min(...orders))
+      // ... find the top field within each segment for each layer
       let topFieldsAllOrders = Promise.all(ordersUp.map((order) => {
         // if the layer includes current order
         if((layer.orders[0] <= order) && (layer.orders[1] >= order)) {
@@ -80,6 +83,7 @@ export default async function CrossScaleNarration(selected, fetchLayerData, laye
               // top field per segment
               const topFields = response.map(d => {
                 let topField = layer.fieldChoice(d)
+                // store layer as integer
                 d.layer = i
                 d.topField = topField
                 return d
@@ -95,17 +99,21 @@ export default async function CrossScaleNarration(selected, fetchLayerData, laye
         }
       }))
       return topFieldsAllOrders.then((response) => {
+        // flatten for easy lookup in following steps
         return response.filter(d => d !== null).flat()
       })
     }))
 
-    // find the best score for each segment
+    // find the best score for each segment across layers
     let topFieldsAcrossLayers = topFieldsAllLayers.then((response) => {
       let layerData = response
+      // the most segments in any layer
       let numSegments = Math.max(...layerData.map(d => d.length))
       let topFieldsAcrossLayers = new Array(numSegments).fill(null)
+      // find the layer with the max score for each segment
       for(let i = 0; i < numSegments; i++) {
         let topLayerForSegment = layerData.map(d => d[i]).sort((a,b) => {return b.topField.value - a.topField.value})[0]
+        // replace integer with layer info
         topLayerForSegment.layer = layers[topLayerForSegment.layer]
         topFieldsAcrossLayers[i] = topLayerForSegment
       }
@@ -121,22 +129,22 @@ export default async function CrossScaleNarration(selected, fetchLayerData, laye
       let bestPathThroughNode = new Array(numSegments).fill(null).map(d => [])
       for(let c = 1; c < numSegments; c++) {
         let p = Math.floor((c - 1) / 4)
-        tree[p].push(c);
-        tree[c].push(p);
+        tree[p].push(c)
+        tree[c].push(p)
       }
 
+      // function to traverse the tree and find the best path
       let searchTree = (segmentData, tree, i, parent) => { 
-        scoresThroughNode[i] = segmentData[i].topField.value; 
+        scoresThroughNode[i] = segmentData[i].topField.value
         var maxScore = 0; 
-        // traverse the tree 
         for(var child of tree[i]) { 
-          // if child is parent, then we continue without recursing further 
+          // move on if no new nodes
           if (child == parent){
             continue
           }
-          // continue to traverse tree 
-          searchTree(segmentData, tree, child, i); 
-          // store the maxScore of previous visited node and present visited node 
+          // go deeper into the tree 
+          searchTree(segmentData, tree, child, i)
+          // update max score and path
           if(scoresThroughNode[child] > maxScore) {
             maxScore = scoresThroughNode[child]
             if(bestPathThroughNode[child].length == 0) {
@@ -145,13 +153,15 @@ export default async function CrossScaleNarration(selected, fetchLayerData, laye
             bestPathThroughNode[i] = [segmentData[i], ...bestPathThroughNode[child]]
           }
         } 
-        // add the maxScore value returned to the parent node 
-        scoresThroughNode[i] += maxScore; 
-    }
+        // add score through current node
+        scoresThroughNode[i] += maxScore
+      }
+      // find best path
       searchTree(segmentData, tree, 0, -1)
       console.log("best path", bestPathThroughNode[0])
       let bestPath = bestPathThroughNode[0].map(d => {
         let field = d.topField
+        // only keep stations with significant scores
         if(field.value !== null) {
           field.color = d.layer.fieldColor(field.field)
           return {
