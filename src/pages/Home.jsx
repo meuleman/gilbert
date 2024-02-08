f3a0f57a01100769072865dc01def803daa98d25
Ian Johnson <enjalot@gmail.com>
2024-02-08 15:17:24 -0500

    pull layer imports into own file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,37 +29,18 @@ import SVGChromosomeNames from '../components/SVGChromosomeNames'
 // import SVGBBox from '../components/SVGBBox'
 
 // layer configurations
-import Bands from '../layers/bands'
-import GCContent from '../layers/gc_content'
-import GeneCounts from '../layers/gene_counts'
-import Nucleotides from '../layers/nucleotides'
-import DHS_OE_Chi from '../layers/dhs_oe_chi'
-import DHS_Components_Sfc from '../layers/dhs_components_sfc'
+import layers from '../layers'
+// CSN layers
 import DHS_Components_Sfc_max from '../layers/dhs_components_sfc_max'
-import DHS_Mean_Signal from '../layers/dhs_meansignal'
-import DHS_Density from '../layers/dhs_density'
-// import Chromatin_OE_Chi from '../layers/chromatin_oe_chi'
-import Chromatin_States_Sfc from '../layers/chromatin_states_sfc'
 import Chromatin_States_Sfc_max from '../layers/chromatin_states_sfc_max';
-// import TF_Motifs_OE_Chi from '../layers/tf_motifs_oe_chi'
-import TF_Motifs_Sfc from '../layers/tf_motifs_sfc'
 import TF_Motifs_Sfc_max from '../layers/tf_motifs_sfc_max'
-import DHS_mapped_TF_motifs_sfc from '../layers/dhs_mapped_tf_motifs_sfc'
-import DHS_mapped_TF_motifs_sfc_max from '../layers/dhs_mapped_tf_motifs_sfc_max'
-import UKBB from '../layers/ukbb'
-import UKBB_Counts from '../layers/ukbb_counts'
-import Repeats_Sfc from '../layers/repeats_sfc'
 import Repeats_Sfc_max from '../layers/repeats_sfc_max'
-import CpG_Island_Density from '../layers/cpg_islands_density'
-import ENCSR000EOT from '../layers/encode_ENCSR000EOT_max'
-import CD3 from '../layers/encode_CD3_D2_Stim_AG90658_output_2.5.1_max'
-import CD3Fiberseq from '../layers/fiberseq_CD3_HAP1d2stim_mean'
-import LADs from '../layers/lads'
-// import DHS_Coreg_2500 from '../layers/dhs_coreg_2500'
-// import DHS_Coreg_Multiscale from '../layers/dhs_coregMultiscale'
-// import DHS_Coreg_Best_Scale_max from '../layers/dhs_coregBestScale_max'
+
 // autocomplete
 import Autocomplete from '../components/Autocomplete/Autocomplete'
+
+import GilbertLogo from '../assets/gilbert-logo.svg?react'
+
 // region SimSearch
 import SimSearchRegion from '../components/SimSearch/SimSearchRegion'
 import SimSearchByFactor from '../components/SimSearch/SimSearchByFactor'
@@ -68,63 +49,15 @@ import DisplaySimSearchRegions from '../components/SimSearch/DisplaySimSearchReg
 import DisplayedExampleRegions from '../components/ExampleRegions/DisplayExampleRegions';
 
 import { getSet } from '../components/Regions/localstorage'
-import RegionFilesSelect from '../components/Regions/RegionFilesSelect'
 import SelectedModal from '../components/SelectedModal'
 import SelectedModalSimSearch from '../components/SimSearch/SelectedModalSimSearch'
 import NarrateRegion from '../components/Narration/NarrateRegion'
 import CrossScaleNarration from '../components/Narration/CrossScaleNarration'
-import LayerSuggestion from '../components/Narration/LayerSuggestion'
 import SelectedModalNarration from '../components/Narration/SelectedModalNarration'
 import GenesetEnrichment from '../components/SimSearch/GenesetEnrichment';
-import Spectrum from '../components/Spectrum';
+// import Spectrum from '../components/Spectrum';
 
-import GilbertLogo from '../assets/gilbert-logo.svg?react'
 
-const layers = [
-  Bands,
-  GeneCounts,
-  GCContent,
-  Nucleotides,
-  DHS_OE_Chi,
-  DHS_Components_Sfc,
-  DHS_Components_Sfc_max,
-  DHS_Mean_Signal,
-  DHS_Density,
-  // DHS_Coreg_2500,
-  // DHS_Coreg_Multiscale,
-  // DHS_Coreg_Best_Scale_max,
-  // Chromatin_OE_Chi,
-  Chromatin_States_Sfc,
-  Chromatin_States_Sfc_max,
-  // TF_Motifs_OE_Chi,
-  TF_Motifs_Sfc,
-  TF_Motifs_Sfc_max,
-  DHS_mapped_TF_motifs_sfc,
-  DHS_mapped_TF_motifs_sfc_max,
-  Repeats_Sfc,
-  Repeats_Sfc_max,
-  UKBB,
-  UKBB_Counts,
-  CpG_Island_Density,
-  ENCSR000EOT,
-  CD3,
-  CD3Fiberseq,
-  LADs,
-]
-
-// const initialLayerOrder = {
-//   4: Bands,
-//   5: DHS_Components_Sfc,
-//   6: DHS_Components_Sfc,
-//   7: Chromatin_States_Sfc,
-//   8: Chromatin_States_Sfc,
-//   9: TF_Motifs_OE_Chi,
-//   10: TF_Motifs_OE_Chi,
-//   11: GCContent,
-//   12: GCContent,
-//   13: GCContent,
-//   14: Nucleotides,
-// }
 
 // declare globally so it isn't recreated on every render
 const debounceTimed = debouncerTimed()
@@ -195,7 +128,7 @@ function Home() {
 
   const [layerLock, setLayerLock] = useState(false)
   const [layerLockFromIcon, setLayerLockFromIcon] = useState(null)
-  const [layer, setLayer] = useState(Bands)
+  const [layer, setLayer] = useState(layers[0])
   function handleLayer(l) {
     setLayer(l)
     setLayerLock(true)
