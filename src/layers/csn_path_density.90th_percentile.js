a3f02bb6889473f9a96fe1cbcd52d69bd4042ff5
camerontrader <camerondtrader@gmail.com>
2024-08-07 09:27:02 -0700

    Update orders array in CSN Path Density layer

--- src/layers/csn_path_density.90th_percentile.js
+++ src/layers/csn_path_density.90th_percentile.js
@@ -6,7 +6,7 @@ export default {
   name: "CSN Path Density (90th Percentile)",
   datasetName: "precomputed_csn_path_density_above_90th_percentile",
   baseURL: `${constants.baseURLPrefix}/20240806`,
-  orders: [4,11],
+  orders: [4,12],
   renderer: "CanvasOpacityValue",
   fieldChoice: d => ({ 
     field: "fraction_of_paths", 
