328172ae5a5190dc629cbffae8a2a8acd379a8e4
Ian Johnson <enjalot@gmail.com>
2024-08-23 09:51:32 -0400

    add clear region callback

--- src/components/Regions/RegionSetModal.jsx
+++ src/components/Regions/RegionSetModal.jsx
@@ -16,6 +16,7 @@ const RegionSetModal = ({
   queryRegions = null,
   queryLoading = "",
   onNumSegments = () => {},
+  onClearRegion = () => {},
 } = {}) => {
 
   const { filters } = useContext(FiltersContext)
