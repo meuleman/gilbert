8618d6a4facd2f49e922a13b53d942e81ad8bebe
Ian Johnson <enjalot@gmail.com>
2024-10-07 10:28:55 -0400

    sort active regions first by bed score then by path

--- src/components/Regions/RegionsProvider.jsx
+++ src/components/Regions/RegionsProvider.jsx
@@ -263,8 +263,9 @@ const RegionsProvider = ({ children }) => {
             let hydrated = tpr.map(d => rehydrateCSN(d, [...csnLayers, ...variantLayers]))
             // combine the regions with the paths so we can sort them by the path score
             let combined = activeRegions.map((r,i) => {
-              return { i, r, p: hydrated[i], score: hydrated[i]?.score }
-            }).sort((a,b) => b.score - a.score)
+              // return { i, r, p: hydrated[i], score: r.score ? r.score : hydrated[i]?.score }
+              return { i, r, p: hydrated[i], rscore: r.score, pscore: hydrated[i]?.score }
+            }).sort((a,b) => (a.rscore && b.rscore && a.rscore.toFixed(3) != b.rscore.toFixed(3)) ? b.rscore - a.rscore : b.pscore - a.pscore)
 
             let reorderedRegions = combined.map(d => d.r)
             let reorderedPaths = combined.map(d => d.p).filter(d => !!d)
