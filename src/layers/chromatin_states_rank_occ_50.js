d7969b32cf0a4de17de7accb47fab7d317e3038b
camerontrader <camerondtrader@gmail.com>
2024-11-27 17:25:14 -0800

    50% overlap chromatin states layer

---
+++ src/layers/chromatin_states_rank_occ_50.js
@@ -0,0 +1,50 @@
+import { scaleOrdinal } from "d3-scale";
+import * as constants from "../lib/constants";
+
+// const fullFields = ["Active TSS","Flanking TSS","Flanking TSS Upstream","Flanking TSS Downstream","Strong transcription","Weak transcription","Genic Enhancer 1","Genic Enhancer 2","Active Enhancer 1","Active Enhancer 2","Weak Enhancer","ZNF genes + repeats","Heterochromatin","Bivalent/Poised TSS","Bivalent Enhancer","Repressed PolyComb","Weak Repressed PolyComb","Quiescent/Low"]
+// const csColors = ["#ff0000","#ff4500","#ff4500","#ff4500","#008000","#006400","#c2e105","#c2e105","#ffc34d","#ffc34d","#ffff00","#66cdaa","#8a91d0","#cd5c5c","#bdb76b","#808080","#c0c0c0","#eeeeee"]  // temp change white to gray-white
+
+export default {
+  name: "Chromatin States (OCC, Ranked, 50% Overlap)",
+  datasetName: "cs_rank_occ_a",
+  // baseURL: "https://storage.googleapis.com/fun-data/hilbert/chromosomes_new",
+  baseURL: `${constants.baseS3URLPrefix}/20241127`,
+  orders: [10,12],
+  renderer: "CanvasScaledValue",
+  fieldChoice: decodeValue,
+  fieldColor: scaleOrdinal()
+    .domain(
+      ["Active TSS","Flanking TSS","Flanking TSS Upstream","Flanking TSS Downstream","Strong transcription","Weak transcription","Genic Enhancer 1","Genic Enhancer 2","Active Enhancer 1","Active Enhancer 2","Weak Enhancer","ZNF genes + repeats","Heterochromatin","Bivalent/Poised TSS","Bivalent Enhancer","Repressed PolyComb","Weak Repressed PolyComb","Quiescent/Low"]
+      )
+    .range(
+      ["#ff0000","#ff4500","#ff4500","#ff4500","#008000","#006400","#c2e105","#c2e105","#ffc34d","#ffc34d","#ffff00","#66cdaa","#8a91d0","#cd5c5c","#bdb76b","#808080","#c0c0c0","#eeeeee"]  // temp change white to gray-white
+      ) 
+    .unknown("#eee"),
+  // used for the base canvas rendering
+  strokeWidthMultiplier: 0.05,
+  stroke: "gray",
+  fill: "white",
+  topValues: true
+}
+
+
+function decodeValue(d) {
+  const fullFields = ["Active TSS","Flanking TSS","Flanking TSS Upstream","Flanking TSS Downstream","Strong transcription","Weak transcription","Genic Enhancer 1","Genic Enhancer 2","Active Enhancer 1","Active Enhancer 2","Weak Enhancer","ZNF genes + repeats","Heterochromatin","Bivalent/Poised TSS","Bivalent Enhancer","Repressed PolyComb","Weak Repressed PolyComb","Quiescent/Low"]
+  let data = d.data;
+  if(!data) return { field: "", value: null }
+  let top = {
+    field: fullFields[data.max_field],
+    value: data.max_value
+  }
+  // let top = Object.keys(data).map((f) => ({
+  //   field: f,
+  //   value: data[f]
+  // })).sort((a,b) => b.value - a.value)[0]
+  // if(!top && d.i == 31240636) {
+  //   console.log("NO TOP", d)
+  //   return { field: "", value: null }
+  // }
+  if(!top) return { field: "", value: null }
+  if(top.value <= 0) return { field: "", value: null }
+  return top
+}
