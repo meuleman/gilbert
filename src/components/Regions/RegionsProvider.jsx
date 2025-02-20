40f650d4ef40e2356b82d7b5f37ccad0e314b294
camerontrader <camerondtrader@gmail.com>
2025-02-19 17:08:08 -0800

    Remove 'GOBP' from region set query

--- src/components/Regions/RegionsProvider.jsx
+++ src/components/Regions/RegionsProvider.jsx
@@ -358,7 +358,7 @@ const RegionsProvider = ({ children }) => {
     // parse enriched genesets and genes and add to query
     if(activeGenesetEnrichment?.length) {
       let genesetsToInclude = activeGenesetEnrichment?.sort((a, b) => a.p - b.p).slice(0, 3)
-      query += "; " + genesetsToInclude.map(d => `GO ${d.geneset.replace(/_/g, " ")}`).join("; ")
+      query += "; " + genesetsToInclude.map(d => `GO ${d.geneset.split("_").slice(1).join(" ")}`).join("; ")
       // collect genes
       let genesetGenes = genesetsToInclude.flatMap(d => d.genes)
       let regionGenes = narrations.flatMap(d => d.genes.map(g => ({name: g.name, inGene: g.in_gene})))
