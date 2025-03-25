6bf83fde9f17a0de931d68613159d9f877c40e5a
Wouter Meuleman <meuleman@gmail.com>
2025-03-25 20:21:31 +0100

    presumably an optimized version of spectrum rendering w.r.t. width calculations

--- src/components/Narration/Spectrum.jsx
+++ src/components/Narration/Spectrum.jsx
@@ -4,38 +4,35 @@ import genesetOrder from '../../data/genesetOrder2023.json';
 import colors from '../../data/spectrumColors.json';
 import labels from '../../data/spectrumLabels.json';
 import Loading from '../Loading';
-import * as d3 from 'd3'
+import * as d3 from 'd3';
 import RegionsContext from '../Regions/RegionsContext';
-import SelectedStatesStore from '../../states/SelectedStates'
-import scaleCanvas from '../../lib/canvas'
-// import Tooltip from './Tooltips/Tooltip';
-
+import SelectedStatesStore from '../../states/SelectedStates';
+import scaleCanvas from '../../lib/canvas';
 
 const Tooltip = ({ geneset, x, y, visible, width }) => {
   if (!visible || !geneset) return null;
-  let genesetName = geneset.geneset.split("_").slice(1).join(" ").toLowerCase()
-  genesetName = genesetName.charAt(0).toUpperCase() + genesetName.slice(1)
-  let enrichment = Math.round(geneset.score * 10000) / 10000
-  let positionLeft = (geneset?.index / genesetOrder.length) < 0.5 
+  let genesetName = geneset.geneset.split("_").slice(1).join(" ").toLowerCase();
+  genesetName = genesetName.charAt(0).toUpperCase() + genesetName.slice(1);
+  let enrichment = Math.round(geneset.score * 10000) / 10000;
+  const positionLeft = (geneset?.index / genesetOrder.length) < 0.5;
   const tooltipRef = useRef();
 
   const content = (
-  <div>
     <div>
-      {genesetName}
+      <div>{genesetName}</div>
+      {(enrichment > 0) && (
+        <div>-log10(p): {enrichment}</div>
+      )}
     </div>
-    {(enrichment > 0) &&
-      <div>
-        -log10(p): {enrichment}
-      </div> 
-    }
-  </div>)
+  );
 
   useEffect(() => {
     if (tooltipRef.current) {
       tooltipRef.current.style.left = 'auto';
       tooltipRef.current.style.right = 'auto';
-      positionLeft ? tooltipRef.current.style.left = `${x}px` : tooltipRef.current.style.right = `${width - x}px`;
+      positionLeft 
+        ? tooltipRef.current.style.left = `${x}px` 
+        : tooltipRef.current.style.right = `${width - x}px`;
       tooltipRef.current.style.bottom = `${y}px`;
       tooltipRef.current.style.display = visible ? 'block' : 'none';
       tooltipRef.current.style.minWidth = '150px';
@@ -48,7 +45,7 @@ const Tooltip = ({ geneset, x, y, visible, width }) => {
       tooltipRef.current.style.fontSize = '12px';
       tooltipRef.current.style.color = 'black';
     }
-  }, [x, y, visible, positionLeft]);
+  }, [x, y, visible, positionLeft, width]);
 
   return (
     <div ref={tooltipRef} style={{ position: 'absolute', background: '#fff', border: '1px solid #ccc', padding: '5px', pointerEvents: 'none' }}>
@@ -68,29 +65,21 @@ const Spectrum = ({
   plotYStart = 20,
   spectrumBarHeight = 10,
   plotYStop = height - spectrumBarHeight,
-  curveHeight = plotYStop - plotYStart,
-  
+  curveHeight = (height - spectrumBarHeight) - plotYStart,
 } = {}) => {
 
   const SpectrumBar = ({ data, ctx, xScale, y, height, colorbarX }) => {
-
-    // Draw the rectangles for the spectrum bar
     data.forEach((d, i) => {
-        ctx.fillStyle = colorbarX(i);
-        ctx.fillRect(xScale(i), y, xScale(1) - xScale(0), height);
+      ctx.fillStyle = colorbarX(i);
+      ctx.fillRect(xScale(i), y, xScale(i + 1) - xScale(i), height);
     });
   };
 
   const Curve = ({ data, ctx, xScale, yScale, height, color }) => {
-
-    // Function to draw the y-axis with ticks
     const drawYAxis = (ctx, yScale, yAxisStart, yAxisStop, estTickCount = 6) => {
-      const tickLength = 5; // Length of the tick marks
-
-      ctx.strokeStyle = '#000'; // Axis color
+      const tickLength = 5; 
+      ctx.strokeStyle = '#000';
       ctx.lineWidth = 1;
-
-      // Draw the y-axis line
       ctx.beginPath();
       ctx.moveTo(xScale(0), yAxisStart);
       ctx.lineTo(xScale(0), yAxisStop);
@@ -98,153 +87,175 @@ const Spectrum = ({
 
       const maxValue = Math.max(...data);
       const minValue = Math.min(...data);
-      const step = Math.ceil((maxValue - minValue) / (estTickCount));
+      const step = Math.ceil((maxValue - minValue) / estTickCount);
       const yTicks = Array.from({ length: estTickCount }, (v, i) => minValue + i * step).filter(d => d <= maxValue);
       
-      // Draw the ticks and labels
       yTicks.forEach((value) => {
-        const y = yScale(value)
-
+        const yCoord = yScale(value);
         ctx.beginPath();
-        ctx.moveTo(xScale(0) - tickLength, y);
-        ctx.lineTo(xScale(0), y);
+        ctx.moveTo(xScale(0) - tickLength, yCoord);
+        ctx.lineTo(xScale(0), yCoord);
         ctx.stroke();
 
-        ctx.fillStyle = '#000'; // Label color
+        ctx.fillStyle = '#000';
         ctx.textAlign = 'right';
         ctx.textBaseline = 'middle';
-        ctx.fillText(Math.floor(value), xScale(0) - tickLength - 2, y);
-      })
+        ctx.fillText(Math.floor(value), xScale(0) - tickLength - 2, yCoord);
+      });
     };
 
-    // Draw the y-axis
     drawYAxis(ctx, yScale, yScale(Math.max(...data)), yScale(0), Math.min(6, Math.max(...data) + 1));
   
-    // Draw the curve
     data.forEach((d, i) => {
       if (d) {
         ctx.fillStyle = color;
         ctx.globalAlpha = 1;
-        ctx.fillRect(xScale(i), yScale(d), xScale(1) - xScale(0), height - yScale(d));
+        ctx.fillRect(xScale(i), yScale(d), xScale(i + 1) - xScale(i), height - yScale(d));
       }
     });
   };
 
-  const Membership = ({membership, genesetOrder, data, ctx, xScale, yScale, height, barWidth, color}) => {
-    console.log("geneset membership length", membership.length)
+  const Membership = ({ membership, genesetOrder, data, ctx, xScale, yScale, height, barWidth, color }) => {
     membership.forEach((d) => {
       if (d.geneset) {
-        let i = genesetOrder.indexOf(d.geneset);
+        const i = genesetOrder.indexOf(d.geneset);
         if(i >= 0) {
-          let value = data[i];
+          const value = data[i];
           ctx.fillStyle = color;
           ctx.globalAlpha = 1;
-          ctx.fillRect(xScale(i), yScale(value), xScale(barWidth) - xScale(0), height - yScale(value));
+          ctx.fillRect(xScale(i), yScale(value), xScale(i + barWidth) - xScale(i), height - yScale(value));
         }
       }
     });
-  }
+  };
 
   const Labels = ({ labels, ctx, xScale }) => {
     ctx.font = '7px Arial';
     ctx.fillStyle = '#000';
     ctx.textAlign = "left";
     ctx.textBaseline = "top";
-
-    labels.forEach((d, i) => {
+    labels.forEach((d) => {
       ctx.fillText(d.label, xScale(d.i), 0);
     });
+  };
 
-  }
-
-  const { activeGenesetEnrichment } = useContext(RegionsContext)
-  const { selectedGenesetMembership } = SelectedStatesStore()
+  const { activeGenesetEnrichment } = useContext(RegionsContext);
+  const { selectedGenesetMembership } = SelectedStatesStore();
 
   const [enrichments, setEnrichments] = useState(new Array(genesetOrder.length).fill(0));
   const [smoothData, setSmoothData] = useState(new Array(genesetOrder.length).fill(0));
-
   const [tooltip, setTooltip] = useState({ content: null, x: 0, y: 0, visible: false });
 
   useEffect(() => {
     if(activeGenesetEnrichment?.length) {
+      const newEnrichments = new Array(genesetOrder.length).fill(0);
+      const enrichmentsMax = new Array(genesetOrder.length).fill(0);
+      const enrichmentsSmooth = new Array(genesetOrder.length).fill(0);
 
-      // console.time("INIT")
-      let enrichments = new Array(genesetOrder.length).fill(0)
-      let enrichmentsMax = new Array(genesetOrder.length).fill(0)
-      let enrichmentsSmooth = new Array(genesetOrder.length).fill(0)
-      // console.timeEnd("INIT")
-
-      // console.time("FILL")
-      activeGenesetEnrichment.forEach((d) => enrichments[genesetOrder.indexOf(d.geneset)] = -Math.log10(d.p))
-      setEnrichments(enrichments)
-      // console.timeEnd("FILL")
-      // console.log("FILL", Math.max(...enrichments))
-      
-      // console.time("MAX")
-      enrichments.forEach((e, i) => {
-        let startIndex = Math.max(0, i - windowSize / 2);
-        let endIndex = Math.min(enrichments.length, i + windowSize / 2);
-        let enrichmentsInWindow = enrichments.slice(startIndex, endIndex);
-        enrichmentsMax[i] = Math.max(...enrichmentsInWindow);
+      activeGenesetEnrichment.forEach((d) => {
+        newEnrichments[genesetOrder.indexOf(d.geneset)] = -Math.log10(d.p);
       });
-      // console.timeEnd("MAX")
-      // console.log("MAX", Math.max(...enrichmentsMax))
+      setEnrichments(newEnrichments);
 
-      // console.time("SMOOTH")
+      newEnrichments.forEach((e, i) => {
+        const startIndex = Math.max(0, i - windowSize / 2);
+        const endIndex = Math.min(newEnrichments.length, i + windowSize / 2);
+        const windowArr = newEnrichments.slice(startIndex, endIndex);
+        enrichmentsMax[i] = Math.max(...windowArr);
+      });
+
       enrichmentsMax.forEach((e, i) => {
-        let startIndex = Math.max(0, i - windowSize / 2);
-        let endIndex = Math.min(enrichments.length, i + windowSize / 2);
-        let enrichmentsMaxInWindow = enrichmentsMax.slice(startIndex, endIndex);
-        enrichmentsSmooth[i] = enrichmentsMaxInWindow.reduce((a, b) => a + b) / enrichmentsMaxInWindow.length;
+        const startIndex = Math.max(0, i - windowSize / 2);
+        const endIndex = Math.min(enrichmentsMax.length, i + windowSize / 2);
+        const windowArr = enrichmentsMax.slice(startIndex, endIndex);
+        enrichmentsSmooth[i] = windowArr.reduce((a, b) => a + b, 0) / windowArr.length;
       });
-      // console.timeEnd("SMOOTH")
-      // console.log("SMOOTH", Math.max(...enrichmentsSmooth))
-
-      setSmoothData(enrichmentsSmooth)
+      setSmoothData(enrichmentsSmooth);
     } else {
-      setEnrichments(new Array(genesetOrder.length).fill(0))
-      setSmoothData(new Array(genesetOrder.length).fill(0))
+      setEnrichments(new Array(genesetOrder.length).fill(0));
+      setSmoothData(new Array(genesetOrder.length).fill(0));
     }
+  }, [activeGenesetEnrichment, selectedGenesetMembership, windowSize]);
 
-  }, [activeGenesetEnrichment, selectedGenesetMembership, genesetOrder, windowSize]);
+  // Using d3.scaleLinear for the x and y scales
+  const xScale = useMemo(() => {
+    return d3.scaleLinear()
+      .domain([0, genesetOrder.length - 1])
+      .range([plotXStart, plotXStop]);
+  }, [plotXStart, plotXStop]);
 
-  // const handleMouseClick = () => {
-  //   // setQueryGeneset(tooltipData.genesetName);
-  //   console.log(tooltipData.genesetName)
-  // };
+  const xScaleInvert = useCallback((x) => {
+    return Math.floor(d3.scaleLinear()
+      .domain([plotXStart, plotXStop])
+      .range([0, genesetOrder.length - 1])(x));
+  }, [plotXStart, plotXStop]);
 
-  const xScale = useMemo(() => i => plotXStart + (i / (genesetOrder.length - 1)) * (plotXStop - plotXStart), [plotXStart, plotXStop, genesetOrder.length]);
-  const xScaleInvert = useMemo(() => x => (x - plotXStart) / (plotXStop - plotXStart) * (genesetOrder.length - 1), [plotXStart, plotXStop, genesetOrder.length]);
-  const yScale = useMemo(() => d => plotYStart + (curveHeight) * (1 - (d / Math.max(...smoothData))), [smoothData, plotYStart, curveHeight]);
-  // const yScaleInvert = useMemo(() => y => (1 - (y - plotYStart) / curveHeight) * Math.max(...smoothData), [plotYStart, curveHeight, smoothData]);
+  const yScale = useMemo(() => {
+    const maxSmooth = Math.max(...smoothData) || 1;
+    // invert range: larger value -> lower y position
+    return d3.scaleLinear()
+      .domain([0, maxSmooth])
+      .range([plotYStop, plotYStart]);
+  }, [smoothData, plotYStart, plotYStop]);
 
-  const colorbarX = useMemo(() => {
-    return (i) => {
-      let c = colors[i % colors.length];
-      let r = Math.round(c[0] * 255);
-      let g = Math.round(c[1] * 255);
-      let b = Math.round(c[2] * 255);
-      let rgbColor = `rgb(${r}, ${g}, ${b})`;
-      let hsl = d3.hsl(rgbColor);
-      hsl.l = 0.5;
-      return hsl.toString();
-    };
-  }, [colors]);
+  const colorbarX = useMemo(() => (i) => {
+    const c = colors[i % colors.length];
+    const r = Math.round(c[0] * 255);
+    const g = Math.round(c[1] * 255);
+    const b = Math.round(c[2] * 255);
+    const rgbColor = `rgb(${r}, ${g}, ${b})`;
+    const hsl = d3.hsl(rgbColor);
+    hsl.l = 0.5;
+    return hsl.toString();
+  }, []);
 
-  // create canvas
   const canvasRef = useRef(null);
   useEffect(() => {
-    scaleCanvas(canvasRef.current, canvasRef.current.getContext("2d"), width, height)
-  }, [canvasRef, width, height])
+    scaleCanvas(canvasRef.current, canvasRef.current.getContext("2d"), width, height);
+  }, [width, height]);
+
+  // Memoize mouse event handlers
+  const handleMouseLeave = useCallback(() => {
+    setTooltip({ content: null, x: 0, y: 0, visible: false });
+  }, []);
+
+  const determineGeneset = useCallback((x) => {
+    const index = xScaleInvert(x);
+    if (index >= 0 && index < genesetOrder.length) {
+      const hoverWindowSize = 50;
+      const startIndex = Math.max(0, index - hoverWindowSize / 2);
+      const endIndex = Math.min(enrichments.length, index + hoverWindowSize / 2);
+      const windowValues = enrichments.slice(startIndex, endIndex);
+      let topIndexInWindow = { score: 0, index: index };
+      if (Math.max(...windowValues) !== 0) {
+        topIndexInWindow = windowValues
+          .map((v, i) => ({ score: v, index: i + startIndex }))
+          .sort((a, b) => b.score - a.score)[0];
+      }
+      topIndexInWindow.geneset = genesetOrder[topIndexInWindow.index];
+      return topIndexInWindow;
+    }
+    return null;
+  }, [enrichments, genesetOrder, xScaleInvert]);
+
+  const isHoveringSpectrumBar = useCallback((x, y) => {
+    return x >= plotXStart && x <= plotXStop && y >= plotYStop && y <= height;
+  }, [plotXStart, plotXStop, plotYStop, height]);
+
+  const isHoveringCurve = useCallback((x, y, data) => {
+    const index = xScaleInvert(x);
+    if (index >= 0 && index < data.length) {
+      const yValue = yScale(data[index]);
+      return x >= plotXStart && x <= plotXStop && y >= yValue && y <= plotYStop;
+    }
+    return false;
+  }, [plotXStart, plotXStop, yScale, plotYStop, xScaleInvert]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-
-    // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // add items to canvas
     SpectrumBar({ data: smoothData, ctx, xScale, y: plotYStop, height: spectrumBarHeight, colorbarX });
     Curve({ data: smoothData, ctx, xScale, yScale, height: plotYStop, color: selectedGenesetMembership?.length ? "#555" : "#000" });
     Membership({ membership: selectedGenesetMembership, genesetOrder, data: smoothData, ctx, xScale, yScale, height: plotYStop, barWidth: 10, color: "#F00" });
@@ -254,16 +265,14 @@ const Spectrum = ({
       const rect = canvas.getBoundingClientRect();
       const mouseX = e.clientX - rect.left;
       const mouseY = e.clientY - rect.top;
-      
-      // determine which section of spectrum is hovered over and adjust content accordingly
-      if (isHoveringSpectrumBar(mouseX, mouseY)) {  // colorbar
-        let geneset = determineGeneset(mouseX)
-        // set the enrichment score to 0 so it will not be shown
-        geneset['score'] = 0
+
+      if (isHoveringSpectrumBar(mouseX, mouseY)) {
+        const geneset = determineGeneset(mouseX) || {};
+        geneset.score = 0;
         setTooltip({ content: geneset, x: mouseX, y: height - mouseY, visible: true });
-      } else if (isHoveringCurve(mouseX, mouseY, smoothData)) {  // curve
-        let geneset = determineGeneset(mouseX)
-        if(geneset.score !== 0) {
+      } else if (isHoveringCurve(mouseX, mouseY, smoothData)) {
+        const geneset = determineGeneset(mouseX);
+        if (geneset && geneset.score !== 0) {
           setTooltip({ content: geneset, x: mouseX, y: height - mouseY, visible: true });
         } else {
           handleMouseLeave();
@@ -271,10 +280,6 @@ const Spectrum = ({
       } else {
         handleMouseLeave();
       }
-    }
-
-    const handleMouseLeave = () => {
-      setTooltip({ content: null, x: 0, y: 0, visible: false });
     };
 
     canvas.addEventListener('mousemove', handleMouseMove);
@@ -284,52 +289,13 @@ const Spectrum = ({
       canvas.removeEventListener('mousemove', handleMouseMove);
       canvas.removeEventListener('mouseleave', handleMouseLeave);
     };
-  }, [smoothData, xScale, yScale, plotYStop, spectrumBarHeight, colorbarX, labels, selectedGenesetMembership]);
+  }, [smoothData, xScale, yScale, plotYStop, spectrumBarHeight, colorbarX, labels, selectedGenesetMembership, height, isHoveringSpectrumBar, isHoveringCurve, determineGeneset, handleMouseLeave]);
 
-  // determines if mouse hovering over spectrum color bar
-  const isHoveringSpectrumBar = (x, y) => {
-    return x >= plotXStart && x <= plotXStop && y >= plotYStop && y <= height
-  };
-
-  // determines if mouse hovering over spectrum curve
-  const isHoveringCurve = (x, y, data) => {
-    // ensure that the y value is within the curve
-    const index = Math.floor(xScaleInvert(x))
-    if (index >= 0 && index < data.length) {
-      const yValue = yScale(data[index])
-      return x >= plotXStart && x <= plotXStop && y >= yValue && y <= plotYStop
-    } else {
-      return false
-    }
-  }
-
-  // determines geneset to show on hover given the x position
-  const determineGeneset = (x) => {
-    const index = Math.floor(xScaleInvert(x))
-    if (index >= 0 && index < genesetOrder.length) {
-      const hoverWindowSize = 50;
-      let startIndex = Math.max(0, index - hoverWindowSize / 2);
-      let endIndex = Math.min(enrichments.length, index + hoverWindowSize / 2);
-      let windowValues = enrichments.slice(startIndex, endIndex);
-      
-      let topIndexInWindow = {score: 0, index: index}
-      if(Math.max(...windowValues) !== 0) {
-        topIndexInWindow = windowValues.map((v, i) => ({score: v, index: i + startIndex})).sort((a, b) => b.score - a.score)[0]
-      }
-      topIndexInWindow['geneset'] = genesetOrder[topIndexInWindow.index]
-      return topIndexInWindow
-    } else {
-      return null
-    }
-  }
-
-  // console.log("Spectrum render")
   return (
     <div className={"spectrum-component" + (show ? " show": " hide")} >
       <div style={{ position: 'relative', width: width + 'px', height: height + 'px' }}>
         <canvas ref={canvasRef} width={width} height={height}/>
         <Tooltip geneset={tooltip.content} x={tooltip.x} y={tooltip.y} visible={tooltip.visible} width={width}/>
-
       </div>
     </div>
   );
